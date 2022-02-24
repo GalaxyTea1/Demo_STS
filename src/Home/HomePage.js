e0f38c6d4028d6577dac53ff946d38edef109a92
Trong Dang <trong_1751220006@dau.edu.vn>
2022-02-24 19:56:08 +0700

    fix

--- src/Home/HomePage.js
+++ src/Home/HomePage.js
@@ -110,6 +110,55 @@ function HomePage() {
                             </div>
                         </li>
                     </ul>
+                    {/* nav-mobile */}
+                    <label for='nav-mobile-input' className='nav-mobile'>
+                        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'>
+                            <path d='M0 96C0 78.33 14.33 64 32 64H416C433.7 64 448 78.33 448 96C448 113.7 433.7 128 416 128H32C14.33 128 0 113.7 0 96zM0 256C0 238.3 14.33 224 32 224H416C433.7 224 448 238.3 448 256C448 273.7 433.7 288 416 288H32C14.33 288 0 273.7 0 256zM416 448H32C14.33 448 0 433.7 0 416C0 398.3 14.33 384 32 384H416C433.7 384 448 398.3 448 416C448 433.7 433.7 448 416 448z' />
+                        </svg>
+                    </label>
+                    <input
+                        type='checkbox'
+                        name=''
+                        className='nav-input'
+                        id='nav-mobile-input'
+                        hidden
+                    ></input>
+                    <label for='nav-mobile-input' className='nav-overlay'></label>
+
+                    <div className='nav-moblie-item'>
+                        <label for='nav-mobile-input' className='nav-close'>
+                            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 320 512'>
+                                <path d='M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z' />
+                            </svg>
+                        </label>
+                        <ul className='nav-mobile-list'>
+                            <Link to='/home'>
+                                <li className='item-mobile'>
+                                    <i class='bx bx-home'></i>Home
+                                </li>
+                            </Link>
+                            <Link to='/home'>
+                                <li className='item-mobile'>
+                                    <i class='bx bx-leaf'></i>Features
+                                </li>
+                            </Link>
+                            <Link to='/home'>
+                                <li className='item-mobile'>
+                                    <i class='bx bx-info-circle'></i>About
+                                </li>
+                            </Link>
+                            <Link to='/home'>
+                                <li className='item-mobile'>
+                                    <i class='bx bxl-meta'></i>Contact
+                                </li>
+                            </Link>
+                            <Link to='/home'>
+                                <li className='item-mobile' onClick={() => dispatch(logout())}>
+                                    <i class='bx bx-log-out'></i>Logout
+                                </li>
+                            </Link>
+                        </ul>
+                    </div>
                 </nav>
             </div>
             <div className='banner'>
@@ -123,32 +172,32 @@ function HomePage() {
                     <i className='bx bx-star'></i>Pet List<i className='bx bx-star'></i>
                 </h3>
             </div>
-            <div className='cat-list'>
+            <div className='cat-list-main'>
                 <div className='cat-list'>{renderDataView(state.dataMix.slice(0, 8))}</div>
             </div>
             <div id='footer'>
                 <div className='social-list'>
-                    <Link to=''>
+                    <a href='https://www.facebook.com/Kim.Nguyet.Da.1999/'>
                         <i className='bx bxl-facebook'></i>
-                    </Link>
-                    <Link to=''>
+                    </a>
+                    <a href='https://www.instagram.com/kim_nguyet_da_1999/'>
                         <i className='bx bxl-instagram'></i>
-                    </Link>
-                    <Link to=''>
+                    </a>
+                    <a href='https://www.youtube.com/channel/UCoX_mKXFf7kE6-QCjNdA0IA'>
                         <i className='bx bxl-youtube'></i>
-                    </Link>
-                    <Link to=''>
+                    </a>
+                    <a href='https://www.pinterest.com/'>
                         <i className='bx bxl-pinterest-alt'></i>
-                    </Link>
-                    <Link to=''>
+                    </a>
+                    <a href='https://twitter.com/?lang=vi'>
                         <i className='bx bxl-twitter'></i>
-                    </Link>
-                    <Link to=''>
+                    </a>
+                    <a href='https://www.linkedin.com/in/tr%E1%BB%8Dng-%C4%91%E1%BA%B7ng-856735229/'>
                         <i className='bx bxl-linkedin'></i>
-                    </Link>
+                    </a>
                 </div>
                 <div className='copyright mt-16'>
-                    Power by <Link to=''>Trong</Link>
+                    Power by <a href='https://www.facebook.com/Kim.Nguyet.Da.1999/'>Trong</a>
                 </div>
             </div>
         </div>
